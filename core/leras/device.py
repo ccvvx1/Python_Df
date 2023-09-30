8b2bd7135e8d90c6868b235b9716cac8a5264104
tmw <694497013@qq.com>
2023-09-30 11:37:15 +0800

    feat: leras add chin lines

--- core/leras/device.py
+++ core/leras/device.py
@@ -7,41 +7,51 @@ import time
 from pathlib import Path
 from core.interact import interact as io
 
-
+# 导入必要的库和模块
+# 这段代码定义了两个类：Device 和 Devices。
+# Device 类表示一个具体的计算设备，它包含设备的索引、类型、名称、总内存和可用内存等信息。
+# Devices 类表示一组计算设备，它包含多个 Device 对象。该类提供了一些方法，如获取设备数量、按索引访问设备、获取可用内存最多的设备以及获取可用内存最少的设备。
+# 请注意，这段代码的注释已经包含了详细的解释。此外，代码中还导入了一些外部库和模块，但这些库和模块的具体用途在代码中并没有详细说明。如果需要更多关于这些库和模块的信息，你可能需要查看导入语句所在的上下文来了解它们的用途。
 class Device(object):
     def __init__(self, index, tf_dev_type, name, total_mem, free_mem):
-        self.index = index
-        self.tf_dev_type = tf_dev_type
-        self.name = name
-        
-        self.total_mem = total_mem
-        self.total_mem_gb = total_mem / 1024**3
-        self.free_mem = free_mem
-        self.free_mem_gb = free_mem / 1024**3
+        # 初始化设备对象
+        self.index = index  # 设备索引
+        self.tf_dev_type = tf_dev_type  # 设备类型
+        self.name = name  # 设备名称
+        self.total_mem = total_mem  # 总内存大小（以字节为单位）
+        self.total_mem_gb = total_mem / 1024**3  # 总内存大小（以GB为单位）
+        self.free_mem = free_mem  # 可用内存大小（以字节为单位）
+        self.free_mem_gb = free_mem / 1024**3  # 可用内存大小（以GB为单位）
 
     def __str__(self):
-        return f"[{self.index}]:[{self.name}][{self.free_mem_gb:.3}/{self.total_mem_gb :.3}]"
+        # 用于在打印时显示设备信息的字符串表示
+        return f"[{self.index}]:[{self.name}][{self.free_mem_gb:.3}/{self.total_mem_gb:.3}]"
 
 class Devices(object):
     all_devices = None
 
     def __init__(self, devices):
+        # 初始化设备集合对象
         self.devices = devices
 
     def __len__(self):
+        # 返回设备集合中设备的数量
         return len(self.devices)
 
     def __getitem__(self, key):
+        # 获取指定索引位置的设备
         result = self.devices[key]
         if isinstance(key, slice):
             return Devices(result)
         return result
 
     def __iter__(self):
+        # 遍历设备集合
         for device in self.devices:
             yield device
 
     def get_best_device(self):
+        # 获取可用内存最多的设备
         result = None
         idx_mem = 0
         for device in self.devices:
@@ -52,6 +62,7 @@ class Devices(object):
         return result
 
     def get_worst_device(self):
+        # 获取可用内存最少的设备
         result = None
         idx_mem = sys.maxsize
         for device in self.devices:
@@ -60,14 +71,15 @@ class Devices(object):
                 result = device
                 idx_mem = mem
         return result
-
     def get_device_by_index(self, idx):
+        # 通过设备索引查找并返回设备对象
         for device in self.devices:
             if device.index == idx:
                 return device
         return None
 
     def get_devices_from_index_list(self, idx_list):
+        # 根据索引列表获取一组设备对象并返回
         result = []
         for device in self.devices:
             if device.index in idx_list:
@@ -75,86 +87,96 @@ class Devices(object):
         return Devices(result)
 
     def get_equal_devices(self, device):
+        # 获取与给定设备具有相同名称的设备列表
         device_name = device.name
         result = []
         for device in self.devices:
             if device.name == device_name:
-                result.append (device)
+                result.append(device)
         return Devices(result)
 
     def get_devices_at_least_mem(self, totalmemsize_gb):
+        # 获取至少具有指定内存大小的设备列表（以GB为单位）
         result = []
         for device in self.devices:
-            if device.total_mem >= totalmemsize_gb*(1024**3):
-                result.append (device)
+            if device.total_mem >= totalmemsize_gb * (1024**3):
+                result.append(device)
         return Devices(result)
 
     @staticmethod
-    def _get_tf_devices_proc(q : multiprocessing.Queue):
-        
+    def _get_tf_devices_proc(q: multiprocessing.Queue):
+        # 静态方法：用于获取TensorFlow中的计算设备信息
+
+        # 在Windows上设置GPU缓存路径（如果适用）
         if sys.platform[0:3] == 'win':
             compute_cache_path = Path(os.environ['APPDATA']) / 'NVIDIA' / ('ComputeCache_ALL')
             os.environ['CUDA_CACHE_PATH'] = str(compute_cache_path)
             if not compute_cache_path.exists():
                 io.log_info("Caching GPU kernels...")
                 compute_cache_path.mkdir(parents=True, exist_ok=True)
-                
+
         import tensorflow
-        
+
         tf_version = tensorflow.version.VERSION
-        #if tf_version is None:
-        #    tf_version = tensorflow.version.GIT_VERSION
         if tf_version[0] == 'v':
             tf_version = tf_version[1:]
         if tf_version[0] == '2':
             tf = tensorflow.compat.v1
         else:
             tf = tensorflow
-        
+
         import logging
-        # Disable tensorflow warnings
+        # 禁用TensorFlow的警告信息
         tf_logger = logging.getLogger('tensorflow')
         tf_logger.setLevel(logging.ERROR)
 
         from tensorflow.python.client import device_lib
 
         devices = []
-        
+
         physical_devices = device_lib.list_local_devices()
         physical_devices_f = {}
         for dev in physical_devices:
             dev_type = dev.device_type
             dev_tf_name = dev.name
-            dev_tf_name = dev_tf_name[ dev_tf_name.index(dev_type) : ]
-            
+            dev_tf_name = dev_tf_name[dev_tf_name.index(dev_type):]
+
             dev_idx = int(dev_tf_name.split(':')[-1])
-            
-            if dev_type in ['GPU','DML']:
+
+            if dev_type in ['GPU', 'DML']:
                 dev_name = dev_tf_name
-                
+
                 dev_desc = dev.physical_device_desc
                 if len(dev_desc) != 0:
                     if dev_desc[0] == '{':
                         dev_desc_json = json.loads(dev_desc)
-                        dev_desc_json_name = dev_desc_json.get('name',None)
+                        dev_desc_json_name = dev_desc_json.get('name', None)
                         if dev_desc_json_name is not None:
                             dev_name = dev_desc_json_name
                     else:
-                        for param, value in ( v.split(':') for v in dev_desc.split(',') ):
+                        for param, value in (v.split(':') for v in dev_desc.split(',')):
                             param = param.strip()
                             value = value.strip()
                             if param == 'name':
                                 dev_name = value
                                 break
-                
+
                 physical_devices_f[dev_idx] = (dev_type, dev_name, dev.memory_limit)
-                        
+
         q.put(physical_devices_f)
         time.sleep(0.1)
-        
+
+# 这些方法的作用如下：
+# get_device_by_index(idx)：通过设备索引查找并返回设备对象。如果找不到匹配的设备，返回 None。
+# get_devices_from_index_list(idx_list)：根据索引列表获取一组设备对象并返回。可以传入一个设备索引的列表，返回匹配索引的设备列表。
+# get_equal_devices(device)：获取与给定设备具有相同名称的设备列表。这可以用于查找同一类型的多个设备。
+# get_devices_at_least_mem(totalmemsize_gb)：获取至少具有指定内存大小（以GB为单位）的设备列表。
+# _get_tf_devices_proc(q: multiprocessing.Queue)：这是一个静态方法，用于获取TensorFlow中的计算设备信息。它在Windows上设置GPU缓存路径，并使用TensorFlow库来获取计算设备的信息。获取的信息包括设备类型、名称和内存限制，并将其放入一个队列中以供其他部分的代码使用。
+# 请注意，这些方法的注释提供了详细的解释，以帮助你理解它们的功能和用途。
         
     @staticmethod
     def initialize_main_env():
+        # 初始化主计算环境的静态方法
         if int(os.environ.get("NN_DEVICES_INITIALIZED", 0)) != 0:
             return
             
@@ -164,7 +186,7 @@ class Devices(object):
         os.environ['TF_DIRECTML_KERNEL_CACHE_SIZE'] = '2500'
         os.environ['CUDA_​CACHE_​MAXSIZE'] = '2147483647'
         os.environ['TF_MIN_GPU_MULTIPROCESSOR_COUNT'] = '2'
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # tf log errors only
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 仅记录TensorFlow错误信息
         
         q = multiprocessing.Queue()
         p = multiprocessing.Process(target=Devices._get_tf_devices_proc, args=(q,), daemon=True)
@@ -183,25 +205,29 @@ class Devices(object):
             os.environ[f'NN_DEVICE_{i}_NAME'] = name
             os.environ[f'NN_DEVICE_{i}_TOTAL_MEM'] = str(total_mem)
             os.environ[f'NN_DEVICE_{i}_FREE_MEM'] = str(total_mem)
-            
-        
+
+# initialize_main_env()：这是一个静态方法，用于初始化主计算环境。它首先检查环境变量 NN_DEVICES_INITIALIZED 是否为0（尚未初始化）。然后，它设置一系列环境变量，包括TensorFlow和CUDA的相关设置，并启动一个多进程来获取计算设备信息（通过调用 _get_tf_devices_proc 方法）。获取的设备信息包括设备类型、名称和总内存，并将其存储在环境变量中。
 
     @staticmethod
     def getDevices():
+        # 获取设备信息的静态方法
         if Devices.all_devices is None:
             if int(os.environ.get("NN_DEVICES_INITIALIZED", 0)) != 1:
-                raise Exception("nn devices are not initialized. Run initialize_main_env() in main process.")
+                raise Exception("nn devices are not initialized. Run initialize_main_env() in the main process.")
             devices = []
-            for i in range ( int(os.environ['NN_DEVICES_COUNT']) ):
-                devices.append ( Device(index=i,
-                                        tf_dev_type=os.environ[f'NN_DEVICE_{i}_TF_DEV_TYPE'],
-                                        name=os.environ[f'NN_DEVICE_{i}_NAME'],
-                                        total_mem=int(os.environ[f'NN_DEVICE_{i}_TOTAL_MEM']),
-                                        free_mem=int(os.environ[f'NN_DEVICE_{i}_FREE_MEM']), )
+            for i in range(int(os.environ['NN_DEVICES_COUNT'])):
+                devices.append(Device(index=i,
+                                    tf_dev_type=os.environ[f'NN_DEVICE_{i}_TF_DEV_TYPE'],
+                                    name=os.environ[f'NN_DEVICE_{i}_NAME'],
+                                    total_mem=int(os.environ[f'NN_DEVICE_{i}_TOTAL_MEM']),
+                                    free_mem=int(os.environ[f'NN_DEVICE_{i}_FREE_MEM']), )
                                 )
             Devices.all_devices = Devices(devices)
 
         return Devices.all_devices
+
+# getDevices()：这是一个静态方法，用于获取设备信息。它首先检查是否已经初始化计算环境（通过检查 NN_DEVICES_INITIALIZED 环境变量），如果没有初始化，则引发异常。然后，它从环境变量中读取设备信息，创建对应的 Device 对象，并将它们存储在 Devices.all_devices 中。如果已经初始化，则直接返回存储的设备信息。
+# 这两个方法的注释提供了详细的解释，以帮助你理解它们的功能和用途。它们用于管理计算设备信息和计算环境的初始化。
 
 """
 
--- core/leras/nn.py
+++ core/leras/nn.py
@@ -26,6 +26,7 @@ import numpy as np
 from core.interact import interact as io
 from .device import Devices
 
+# 导入必要的库和模块
 
 class nn():
     current_DeviceConfig = None
@@ -43,24 +44,25 @@ class nn():
     
     @staticmethod
     def initialize(device_config=None, floatx="float32", data_format="NHWC"):
+        # 初始化深度学习框架和配置参数的静态方法
 
         if nn.tf is None:
             if device_config is None:
                 device_config = nn.getCurrentDeviceConfig()
             nn.setCurrentDeviceConfig(device_config)
 
-            # Manipulate environment variables before import tensorflow
+            # 在导入TensorFlow之前操作环境变量
 
             first_run = False
             if len(device_config.devices) != 0:
                 if sys.platform[0:3] == 'win':
-                    # Windows specific env vars
-                    if all( [ x.name == device_config.devices[0].name for x in device_config.devices ] ):
-                        devices_str = "_" + device_config.devices[0].name.replace(' ','_')
+                    # Windows特定的环境变量
+                    if all([x.name == device_config.devices[0].name for x in device_config.devices]):
+                        devices_str = "_" + device_config.devices[0].name.replace(' ', '_')
                     else:
                         devices_str = ""
                         for device in device_config.devices:
-                            devices_str += "_" + device.name.replace(' ','_')
+                            devices_str += "_" + device.name.replace(' ', '_')
 
                     compute_cache_path = Path(os.environ['APPDATA']) / 'NVIDIA' / ('ComputeCache' + devices_str)
                     if not compute_cache_path.exists():
@@ -84,7 +86,7 @@ class nn():
                 tf = tensorflow
 
             import logging
-            # Disable tensorflow warnings
+            # 禁用TensorFlow的警告信息
             tf_logger = logging.getLogger('tensorflow')
             tf_logger.setLevel(logging.ERROR)
             
@@ -92,7 +94,7 @@ class nn():
                 tf.disable_v2_behavior()
             nn.tf = tf
 
-            # Initialize framework
+            # 初始化深度学习框架
             import core.leras.ops
             import core.leras.layers
             import core.leras.initializers
@@ -100,7 +102,7 @@ class nn():
             import core.leras.models
             import core.leras.archis
             
-            # Configure tensorflow session-config
+            # 配置TensorFlow会话选项
             if len(device_config.devices) == 0:
                 config = tf.ConfigProto(device_count={'GPU': 0})
                 nn.tf_default_device_name = '/CPU:0'
@@ -124,66 +126,123 @@ class nn():
         else:
             raise ValueError(f"unsupported floatx {floatx}")
         nn.set_floatx(floatx)
-        nn.set_data_format(data_format)
+        nn.set_data_format(data_format)  
+
+        # 这段代码定义了一个名为 nn 的类，用于初始化深度学习框架（例如TensorFlow）以及配置相关参数。
+        # initialize(device_config=None, floatx="float32", data_format="NHWC")：这是一个静态方法，用于初始化深度学习框架和配置参数。它接受三个可选参数：
+        # device_config：设备配置对象，用于配置计算设备。
+        # floatx：指定浮点数数据类型（默认为 "float32"）。
+        # data_format：指定数据格式（默认为 "NHWC"）。
+        # 在方法内部，首先检查是否已经初始化TensorFlow和相关参数，如果没有初始化，则进行初始化。
+        # 然后，根据计算设备配置设置相关的环境变量，包括GPU缓存路径等。
+        # 接着，导入TensorFlow库，并设置一些TensorFlow的配置选项，以便在需要时使用GPU。
+        # 最后，根据指定的浮点数数据类型和数据格式进行配置，并初始化深度学习框架。
+        # 这段代码的注释提供了详细的解释，以帮助你理解它的功能和用途。它用于配置深度学习框架和相关参数，以便进行深度学习任务。
 
     @staticmethod
     def initialize_main_env():
+        # 初始化主计算环境的静态方法
         Devices.initialize_main_env()
 
     @staticmethod
     def set_floatx(tf_dtype):
         """
-        set default float type for all layers when dtype is None for them
+        设置默认的浮点数数据类型，适用于所有层当它们的数据类型为 None 时。
         """
         nn.floatx = tf_dtype
 
     @staticmethod
     def set_data_format(data_format):
+        """
+        设置数据格式（data_format）。
+
+        参数：
+        - data_format：数据格式字符串，支持 "NHWC" 或 "NCHW"。
+        """
         if data_format != "NHWC" and data_format != "NCHW":
-            raise ValueError(f"unsupported data_format {data_format}")
+            raise ValueError(f"不支持的数据格式 {data_format}")
         nn.data_format = data_format
 
         if data_format == "NHWC":
             nn.conv2d_ch_axis = 3
-            nn.conv2d_spatial_axes = [1,2]
+            nn.conv2d_spatial_axes = [1, 2]
         elif data_format == "NCHW":
             nn.conv2d_ch_axis = 1
-            nn.conv2d_spatial_axes = [2,3]
+            nn.conv2d_spatial_axes = [2, 3]
 
     @staticmethod
-    def get4Dshape ( w, h, c ):
+    def get4Dshape(w, h, c):
         """
-        returns 4D shape based on current data_format
+        根据当前的数据格式返回 4D 形状。
+
+        参数：
+        - w：宽度
+        - h：高度
+        - c：通道数
+
+        返回值：返回一个形状为 (None, h, w, c) 或 (None, c, h, w) 的元组，具体取决于当前的数据格式。
         """
         if nn.data_format == "NHWC":
-            return (None,h,w,c)
+            return (None, h, w, c)
         else:
-            return (None,c,h,w)
+            return (None, c, h, w)
 
     @staticmethod
-    def to_data_format( x, to_data_format, from_data_format):
+    def to_data_format(x, to_data_format, from_data_format):
+        """
+        将数据从一个数据格式转换为另一个数据格式。
+
+        参数：
+        - x：要转换的数据
+        - to_data_format：目标数据格式字符串，支持 "NHWC" 或 "NCHW"。
+        - from_data_format：原始数据格式字符串，支持 "NHWC" 或 "NCHW"。
+
+        返回值：返回转换后的数据。
+        """
         if to_data_format == from_data_format:
             return x
 
         if to_data_format == "NHWC":
-            return np.transpose(x, (0,2,3,1) )
+            return np.transpose(x, (0, 2, 3, 1))
         elif to_data_format == "NCHW":
-            return np.transpose(x, (0,3,1,2) )
+            return np.transpose(x, (0, 3, 1, 2))
         else:
-            raise ValueError(f"unsupported to_data_format {to_data_format}")
+            raise ValueError(f"不支持的目标数据格式 {to_data_format}")
 
     @staticmethod
     def getCurrentDeviceConfig():
+        """
+        获取当前设备配置对象。
+
+        返回值：返回当前设备配置对象。
+        """
         if nn.current_DeviceConfig is None:
-            nn.current_DeviceConfig = DeviceConfig.BestGPU()
+            nn.current_DeviceConfig = DeviceConfig.BestGPU()  # 假设使用最佳GPU设备配置
         return nn.current_DeviceConfig
+
+    # 这些静态方法提供了一些配置和工具函数，用于设置默认的浮点数数据类型、数据格式，进行数据格式转换，获取设备配置等。它们用于方便地配置和管理深度学习框架的参数和环境。注释提供了详细的解释，以帮助你理解它们的功能和用途。
 
     @staticmethod
     def setCurrentDeviceConfig(device_config):
+        """
+        设置当前的设备配置对象。
+
+        参数：
+        - device_config：设备配置对象，用于指定计算设备。
+
+        用法示例：
+        nn.setCurrentDeviceConfig(device_config)
+        """
         nn.current_DeviceConfig = device_config
 
     @staticmethod
     def reset_session():
+        """
+        重置TensorFlow会话。
+
+        用法示例：
+        nn.reset_session()
+        """
         if nn.tf is not None:
             if nn.tf_sess is not None:
                 nn.tf.reset_default_graph()
@@ -192,6 +251,12 @@ class nn():
 
     @staticmethod
     def close_session():
+        """
+        关闭TensorFlow会话。
+
+        用法示例：
+        nn.close_session()
+        """
         if nn.tf_sess is not None:
             nn.tf.reset_default_graph()
             nn.tf_sess.close()
@@ -199,6 +264,20 @@ class nn():
 
     @staticmethod
     def ask_choose_device_idxs(choose_only_one=False, allow_cpu=True, suggest_best_multi_gpu=False, suggest_all_gpu=False):
+        """
+        提示用户选择计算设备的索引。
+
+        参数：
+        - choose_only_one：是否只能选择一个设备（默认为 False）。
+        - allow_cpu：是否允许选择 CPU 设备（默认为 True）。
+        - suggest_best_multi_gpu：是否建议选择最佳的多GPU配置（默认为 False）。
+        - suggest_all_gpu：是否建议选择所有GPU设备（默认为 False）。
+
+        返回值：返回用户选择的设备索引列表。
+
+        用法示例：
+        indexes = nn.ask_choose_device_idxs(choose_only_one=True)
+        """
         devices = Devices.getDevices()
         if len(devices) == 0:
             return []
@@ -212,56 +291,78 @@ class nn():
         if suggest_all_gpu:
             best_device_indexes = all_devices_indexes
         elif suggest_best_multi_gpu:
-            best_device_indexes = [device.index for device in devices.get_equal_devices(devices.get_best_device()) ]
+            best_device_indexes = [device.index for device in devices.get_equal_devices(devices.get_best_device())]
         else:
-            best_device_indexes = [ devices.get_best_device().index ]
+            best_device_indexes = [devices.get_best_device().index]
         best_device_indexes = ",".join([str(x) for x in best_device_indexes])
 
-        io.log_info ("")
+        io.log_info("")
         if choose_only_one:
-            io.log_info ("Choose one GPU idx.")
+            io.log_info("选择一个GPU索引。")
         else:
-            io.log_info ("Choose one or several GPU idxs (separated by comma).")
-        io.log_info ("")
+            io.log_info("选择一个或多个GPU索引（用逗号分隔）。")
+        io.log_info("")
 
         if allow_cpu:
-            io.log_info ("[CPU] : CPU")
+            io.log_info("[CPU] : CPU")
         for device in devices:
-            io.log_info (f"  [{device.index}] : {device.name}")
+            io.log_info(f"  [{device.index}] : {device.name}")
 
-        io.log_info ("")
+        io.log_info("")
 
         while True:
             try:
                 if choose_only_one:
-                    choosed_idxs = io.input_str("Which GPU index to choose?", best_device_indexes)
+                    choosed_idxs = io.input_str("选择哪个GPU索引?", best_device_indexes)
                 else:
-                    choosed_idxs = io.input_str("Which GPU indexes to choose?", best_device_indexes)
+                    choosed_idxs = io.input_str("选择哪些GPU索引?", best_device_indexes)
 
                 if allow_cpu and choosed_idxs.lower() == "cpu":
                     choosed_idxs = []
                     break
 
-                choosed_idxs = [ int(x) for x in choosed_idxs.split(',') ]
+                choosed_idxs = [int(x) for x in choosed_idxs.split(',')]
 
                 if choose_only_one:
                     if len(choosed_idxs) == 1:
                         break
                 else:
-                    if all( [idx in all_devices_indexes for idx in choosed_idxs] ):
+                    if all([idx in all_devices_indexes for idx in choosed_idxs]):
                         break
             except:
                 pass
-        io.log_info ("")
+        io.log_info("")
 
         return choosed_idxs
+
+
+    # 这些静态方法提供了一些功能，包括设置当前设备配置、重置和关闭TensorFlow会话以及提示用户选择计算设备的索引。注释提供了详细的解释，以帮助你理解它们的功能和用途。这些方法用于配置和管理深度学习框架的计算设备和会话。
 
     class DeviceConfig():
         @staticmethod
         def ask_choose_device(*args, **kwargs):
-            return nn.DeviceConfig.GPUIndexes( nn.ask_choose_device_idxs(*args,**kwargs) )
-        
-        def __init__ (self, devices=None):
+            """
+            提示用户选择计算设备，并返回一个包含所选设备索引的 DeviceConfig 对象。
+
+            参数：与 nn.ask_choose_device_idxs 方法相同的参数。
+
+            返回值：一个包含所选设备索引的 DeviceConfig 对象。
+
+            用法示例：
+            device_config = DeviceConfig.ask_choose_device(choose_only_one=True)
+            """
+            return nn.DeviceConfig.GPUIndexes(nn.ask_choose_device_idxs(*args, **kwargs))
+
+        def __init__(self, devices=None):
+            """
+            初始化 DeviceConfig 对象。
+
+            参数：
+            - devices：设备列表或 Devices 对象（默认为 None）。
+
+            用法示例：
+            device_config = DeviceConfig(devices=my_devices)
+            """
             devices = devices or []
 
             if not isinstance(devices, Devices):
@@ -272,6 +373,14 @@ class nn():
 
         @staticmethod
         def BestGPU():
+            """
+            创建一个 DeviceConfig 对象，包含最佳GPU设备。
+
+            返回值：一个包含最佳GPU设备的 DeviceConfig 对象。
+
+            用法示例：
+            best_config = DeviceConfig.BestGPU()
+            """
             devices = Devices.getDevices()
             if len(devices) == 0:
                 return nn.DeviceConfig.CPU()
@@ -280,6 +389,14 @@ class nn():
 
         @staticmethod
         def WorstGPU():
+            """
+            创建一个 DeviceConfig 对象，包含最差GPU设备。
+
+            返回值：一个包含最差GPU设备的 DeviceConfig 对象。
+
+            用法示例：
+            worst_config = DeviceConfig.WorstGPU()
+            """
             devices = Devices.getDevices()
             if len(devices) == 0:
                 return nn.DeviceConfig.CPU()
@@ -288,6 +405,17 @@ class nn():
 
         @staticmethod
         def GPUIndexes(indexes):
+            """
+            创建一个 DeviceConfig 对象，包含指定索引的GPU设备。
+
+            参数：
+            - indexes：要包含的GPU设备索引列表。
+
+            返回值：一个包含指定GPU设备索引的 DeviceConfig 对象。
+
+            用法示例：
+            gpu_config = DeviceConfig.GPUIndexes([0, 1, 2])
+            """
             if len(indexes) != 0:
                 devices = Devices.getDevices().get_devices_from_index_list(indexes)
             else:
@@ -297,4 +425,14 @@ class nn():
 
         @staticmethod
         def CPU():
+            """
+            创建一个 DeviceConfig 对象，表示仅使用CPU设备。
+
+            返回值：一个表示仅使用CPU设备的 DeviceConfig 对象。
+
+            用法示例：
+            cpu_config = DeviceConfig.CPU()
+            """
             return nn.DeviceConfig([])
+
+    # DeviceConfig 类用于配置计算设备的对象。它包含了一些静态方法，用于创建不同的设备配置。这些方法允许用户选择最佳或最差的GPU设备，或者自定义选择特定的GPU设备。用户还可以创建仅使用CPU设备的配置。这些方法提供了灵活的选项，以满足不同深度学习任务的需求。注释提供了详细的解释，以帮助你理解它们的功能和用途。
